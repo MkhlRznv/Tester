9f8bdd0bbeeede948c9a728c56c6fae4e9ec86bf
Mikhail R <kremrma@gmail.com>
2023-02-05 11:31:42 +0500

    add test demoqa TextBox on lecture

---
+++ src/test/java/qaguru/TextBoxTestsOnLection.java
@@ -0,0 +1,43 @@
+package qaguru;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTestsOnLection {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest(){
+
+        String userName = "Mik Ryazanov";
+        String userEmail = "Mik@gmail.com";
+        String currentAddress = "Some address 1";
+
+        open("/text-box");
+        $(".main-header").shouldHave(text("Text Box"));
+
+        $("#userName").setValue(userName);
+        $("#userEmail").setValue(userEmail);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue("Some address 2");
+        $("#submit").click();
+
+        $("#output").shouldBe(visible);
+        $("#output").$("#name").shouldHave(text(userName));
+        $("#output #email").shouldHave(text(userEmail));
+        $("#output #currentAddress").shouldHave(text(currentAddress));
+
+    }
+
+}
